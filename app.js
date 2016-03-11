d5b8b475938e65a015ccd284710c70002e67272d
Deanne <dmboettch@gmail.com>
2016-03-10 21:25:40 -0700

    Add head.ejs and start styles.css

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var request = require('request');
 var app = express();
 
 app.set('view engine', 'ejs');
+//set public static files (for css)
+app.use(express.static('public'));
 
+app.get('/', function(req,res){
+    res.render('pages/index');
 
+});
 
 
 

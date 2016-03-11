c7863cfd140fc5f0f3dcbbb9bd0b3b0143ac4e40
Deanne <dmboettch@gmail.com>
2016-03-10 19:03:40 -0700

    Create new folders

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+
+
+
+
 console.log('Hello');
+
+app.listen(3000, function(){
+    console.log('Example app listening on port 3000.');
+});

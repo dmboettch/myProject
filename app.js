0e69db21ec41eeafea27ab9b909e4d3209b6579d
Deanne <dmboettch@gmail.com>
2016-03-11 00:13:44 -0700

    Add pages and links on navbar.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var request = require('request');
-
+var port = 8080;
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -9,14 +9,42 @@ app.use(express.static('public'));
 
 app.get('/', function(req,res){
     res.render('pages/index');
-
 });
+
+app.get('/fowl', function(req,res){
+    res.render('pages/fowl');
+});
+
+app.get('/garden', function(req,res){
+    res.render('pages/garden');
+});
+
+app.get('/pest', function(req,res){
+    res.render('pages/pest');
+});
+
+app.get('/herd', function(req,res){
+    res.render('pages/herd');
+});
+
+app.get('/sting', function(req,res){
+    res.render('pages/sting');
+});
+
+app.get('/shop', function(req,res){
+    res.render('pages/shop');
+});
+
+app.get('/contact', function(req,res){
+    res.render('pages/contact');
+});
+
 
 
 
 
 console.log('Hello');
 
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000.');
+app.listen(port, function(){
+    console.log('Example app listening on port' + port);
 });
